71e53d4a15fd280b243b6b7c7af0dfb7076a783f
Radu Ciocan <ciocan@gmail.com>
2023-10-12 20:11:23 +0100

    add shadcn

--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -1,10 +1,11 @@
-import sharedConfig from "@sicap/tailwind-config/tailwind.config.ts";
 // tailwind config is required for editor support
 import type { Config } from "tailwindcss";
+import sharedConfig from "@sicap/tailwind-config/tailwind.config.ts";
 
-const config: Pick<Config, "prefix" | "presets"> = {
+const config: Pick<Config, "prefix" | "presets" | "plugins"> = {
   prefix: "ui-",
   presets: [sharedConfig],
+  plugins: [require("tailwindcss-animate")],
 };
 
 export default config;
