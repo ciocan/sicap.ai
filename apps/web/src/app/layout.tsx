f99bad5e2aaa3b35b8bcc6865cfd15b37cd8367a
Radu Ciocan <ciocan@gmail.com>
2023-10-13 15:48:42 +0100

    linter fix

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
-import "./globals.css";
 import "@sicap/ui/src/styles/styles.css";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
