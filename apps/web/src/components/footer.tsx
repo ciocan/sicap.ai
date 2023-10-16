907f248afac2393652882cc10907b940407be69d
Radu Ciocan <ciocan@gmail.com>
2023-10-16 16:08:30 +0100

    add footer links

--- apps/web/src/components/footer.tsx
+++ apps/web/src/components/footer.tsx
@@ -1,8 +1,25 @@
+"use client";
+import Link from "next/link";
+
+import { Separator } from "@sicap/ui";
+
 export function Footer(): JSX.Element {
   return (
     <footer className="mt-auto">
-      <div className="mx-auto w-full max-w-screen-xl p-6 md:py-8">
-        <h3>footer</h3>
+      <Separator />
+      <div className="mx-auto w-full max-w-screen-xl p-4">
+        <ul className="flex text-primary opacity-60 text-xs justify-center gap-6">
+          <li>
+            <Link href="/confidentialitate" className="hover:underline">
+              Politica de confidentialitate
+            </Link>
+          </li>
+          <li>
+            <Link target="_blank" href="https://cloudify.ro" className="hover:underline">
+              Hosting oferit de Cloudify.ro
+            </Link>
+          </li>
+        </ul>
       </div>
     </footer>
   );
--- packages/ui/src/components/index.tsx
+++ packages/ui/src/components/index.tsx
@@ -4,3 +4,4 @@ import "../styles/styles.css";
 // components
 export * from "./card";
 export * from "./ui/button";
+export * from "./ui/separator";
