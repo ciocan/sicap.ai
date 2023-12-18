511cae856512419cd67a79cdf4a8f4cf05b66365
Radu Ciocan <ciocan@gmail.com>
2023-12-18 11:33:34 +0000

    add index totals to homepage and about page

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -6,7 +6,7 @@ export const revalidate = 0;
 export const dynamic = "force-dynamic";
 
 export default async function Page() {
-  const { licitatii, achizitii } = await getTotal();
+  const { licitatii, achizitii, offline } = await getTotal();
 
   return (
     <main className="my-auto p-4 flex flex-col gap-2 items-center">
@@ -15,9 +15,10 @@ export default async function Page() {
           <span className="font-bold">SICAP</span>.ai
         </h1>
         <h2 className="text-xs text-center">
-          <span className="font-mono font-bold">{formatNumber(licitatii)}</span> licitatii publice
-          si <span className="font-mono font-bold">{formatNumber(achizitii)}</span> achizitii
-          directe indexate
+          <span className="font-mono font-bold">{formatNumber(licitatii)}</span> licitatii publice, {" "}
+          <span className="font-mono font-bold">{formatNumber(achizitii)}</span> achizitii directe
+          si <span className="font-mono font-bold">{formatNumber(offline)}</span> achizitii
+          offline
         </h2>
       </div>
       <div className="px-2 max-w-lg w-full">
